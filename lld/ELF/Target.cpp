9377cf32f53991caa0661606f18dc259807707fd
Chung-Kai Chen <ckchen@realtek.com>
2026-07-21 15:10:40 +0800

    [Haydn 73] LLD: Haydn ELF target (relocation application)
    The linker half of the relocation contract from commit 3: getRelExpr classifies each R_HAYDN_* type and relocate() patches the resolved value into the bundle bit positions (mirroring HaydnRelocLayout from commit 22). Also registers EM_HAYDN in target dispatch and the linker-script OUTPUT_FORMAT parser.
    
    Signed-off-by: Chung-Kai Chen <ckchen@realtek.com>

--- lld/ELF/Target.cpp
+++ lld/ELF/Target.cpp
@@ -72,6 +72,8 @@ void elf::setTarget(Ctx &ctx) {
     return setARMTargetInfo(ctx);
   case EM_AVR:
     return setAVRTargetInfo(ctx);
+  case EM_HAYDN:
+    return setHaydnTargetInfo(ctx);
   case EM_HEXAGON:
     return setHexagonTargetInfo(ctx);
   case EM_LOONGARCH:
